d008dc30b41ed2f378f1144f3a475e8ac6539dca
hayeong922 <Hayeong.Song@colorado.edu>
2017-05-02 15:56:44 -0600

    for collecting data

---
+++ collect/config.py
@@ -0,0 +1,5 @@
+
+consumer_key = 'qEpX9p7eNOXOJmBjaIDEXRzKv'
+consumer_secret = '3DQIBbJEeUUeitYcGc4Kytv0tMpF0Q8TT5RgIAdfWXnvnP1yXw'
+access_token = '832712848442683392-pe5FbVRn7DUMxMyqLBSG0S1vopppmR5'
+access_secret = 'NwrMIZjX6QqwWlLRjk5gp6IWHFt0faEHS9OWONn0CpTyd'
